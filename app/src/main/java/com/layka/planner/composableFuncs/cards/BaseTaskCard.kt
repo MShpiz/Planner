1fbe92ed15f3551f01448794805f86dcb3d2dcfa
Panfilova Maria <reg@panfilova.com>
2024-09-16 15:46:58 +0300

    -

--- app/src/main/java/com/layka/planner/composableFuncs/cards/BaseTaskCard.kt
+++ app/src/main/java/com/layka/planner/composableFuncs/cards/BaseTaskCard.kt
@@ -1,13 +1,16 @@
 package com.layka.planner.composableFuncs.cards
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -17,12 +20,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.substring
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.layka.planner.R
 import java.time.LocalDate
 
 @Composable
@@ -37,10 +42,6 @@ fun BaseCard(
 ) {
     val isDone: MutableState<Boolean> =
         remember { mutableStateOf(done) }
-    val isFullyShown: MutableState<Boolean> =
-        remember {
-            mutableStateOf(false)
-        }
     Column(
         modifier = Modifier
             .padding(end = 10.dp, bottom = 2.dp, start = 5.dp, top = 5.dp)
@@ -54,16 +55,16 @@ fun BaseCard(
             verticalAlignment = Alignment.CenterVertically,
         )
         {
-
-            Checkbox(checked = isDone.value, onCheckedChange = { newDone ->
-                isDone.value = newDone
-                //Log.v("ClickTrack", "clicked the checkBox ${isDone.value}")
-                updateChecked()
-            })
+            Checkbox(
+                checked = isDone.value, onCheckedChange = { newDone ->
+                    isDone.value = newDone
+                    //Log.v("ClickTrack", "clicked the checkBox ${isDone.value}")
+                    updateChecked()
+                })
 
             Text(
                 text = task,
-                fontSize = 15.sp,
+                fontSize = 17.sp,
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1,
                 style = if (!isDone.value) {
@@ -81,26 +82,33 @@ fun BaseCard(
                 modifier = Modifier
                     .weight(1F)
                     .padding(end = 10.dp)
+                    .align(Alignment.CenterVertically)
             )
 
             tagList.forEach {
-                Tag(it.first, it.second)
-            }
-        }
-
-        if (doneDate != null || typeText != null){
-            Row(Modifier.padding(start = 50.dp, bottom = 5.dp)) {
-                if (typeText != null) {
-                    Text(text = typeText, style = TextStyle(color = Color.LightGray, fontSize = 12.sp))
-                }
-                if (doneDate != null && typeText != null)
-                    Text(text = " | ", style = TextStyle(color = Color.LightGray, fontSize = 12.sp))
-                if (doneDate != null) {
-                    Text(text = doneDate.toString(), style = TextStyle(color = Color.LightGray, fontSize = 12.sp))
+                Box(Modifier.align(Alignment.CenterVertically)) {
+                    Tag(it.first, it.second)
                 }
             }
         }
 
+        
+        Row(Modifier.padding(start = 50.dp, bottom = 5.dp)) {
+            Text(text = stringResource(id = R.string.repeats), style = TextStyle(color = Color.Gray, fontSize = 12.sp))
+            if (typeText != null) {
+                Text(text = typeText, style = TextStyle(color = Color.Gray, fontSize = 12.sp))
+            } else {
+                Text(text = stringResource(id = R.string.never), style = TextStyle(color = Color.Gray, fontSize = 12.sp))
+            }
+
+            if (doneDate != null) {
+                VerticalDivider(
+                    Modifier
+                        .height(16.dp)
+                        .padding(3.dp))
+                Text(text = stringResource(id = R.string.done) + doneDate.toString(), style = TextStyle(color = Color.Gray, fontSize = 12.sp))
+            }
+        }
     }
 }
 
@@ -121,9 +129,14 @@ fun Tag(text:String, color: Color) {
         style = TextStyle(color=textColor),
         overflow = TextOverflow.Ellipsis,
         modifier = Modifier
-            .padding(2.dp)
             .clip(RoundedCornerShape(100.dp))
             .background(color)
-            .padding(start = 8.dp, end = 8.dp, bottom = 3.dp, top = 1.dp)
+            .padding(start = 10.dp, end = 10.dp, bottom = 3.dp, top = 3.dp)
     )
+}
+
+@Preview
+@Composable
+fun BaseCardPreview() {
+    BaseCard(task = "AAA", done = false, doneDate = LocalDate.now(), backgroundColor = Color.White, updateChecked = {  }, typeText = "Daily", tagList = listOf(Pair("AAA", Color.Blue)))
 }
